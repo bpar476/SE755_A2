b3794be2dff4edaaf4d1fcf360c93089a3f22d11
Fraser McIntosh <frasermcintosh1@gmail.com>
2018-09-20 16:47:03 +1200

    Add CV and compare results to untuned model

--- src/traffic_bayesian_regression.py
+++ src/traffic_bayesian_regression.py
@@ -1,3 +1,6 @@
+import warnings
+warnings.filterwarnings("ignore", category=FutureWarning)
+
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import BayesianRidge
@@ -5,6 +8,7 @@ from sklearn.preprocessing import Imputer
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.model_selection import GridSearchCV, KFold
 
 fileName = '../datasets/traffic.csv'
 data = pd.read_csv(fileName)
@@ -21,15 +25,51 @@ processed_features = pd.DataFrame(data=full_pipeline.fit_transform(features))
 
 X_train, X_test, y_train, y_test = train_test_split(processed_features, target, test_size=0.1, random_state=1)
 
-regr = BayesianRidge()
-regr.fit(X_train, y_train)
+param_range = [ 10**x for x in range(-2, 0)]
+param_grid = [
+        {'alpha_1': param_range,
+         'alpha_2': param_range,
+         'lambda_1': param_range,
+         'lambda_2': param_range,
+         },
+]
 
-y_pred = regr.predict(X_test)
-y_train_pred = regr.predict(X_train)
+untuned_model = BayesianRidge()
+untuned_model.fit(X_train, y_train)
+y_pred = untuned_model.predict(X_test)
+y_train_pred = untuned_model.predict(X_train)
 
-print(' ')
+print('_____________________UNTUNED MODEL_____________________ ')
 # The coefficients
-print('Coefficients and Intercept are: ', regr.coef_,"   ",regr.intercept_,' respectively')
+print('Coefficients and Intercept are: ', untuned_model.coef_,"   ", untuned_model.intercept_,' respectively')
+# The mean squared error
+print('_________________###################____________________')
+print("Mean squared error for testing data: %.2f"
+      % mean_squared_error(y_test, y_pred))
+# Explained variance score: 1 is perfect prediction
+print('Variance score for testing data: %.2f' % r2_score(y_test, y_pred))
+print('******************************************************* ')
+print("Mean squared error for training data: %.2f"
+      % mean_squared_error(y_train, y_train_pred))
+# Explained variance score: 1 is perfect prediction
+print('Variance score for training data: %.2f' % r2_score(y_train, y_train_pred))
+
+model = BayesianRidge()
+inner_cv = KFold(n_splits=3, shuffle=True, random_state=1)
+grid_search = GridSearchCV(model, param_grid, cv=inner_cv, scoring='neg_mean_squared_error',verbose=2)
+grid_search.fit(X_train, y_train)
+
+cv_results_ = grid_search.cv_results_
+best_params_ = grid_search.best_params_
+tuned_model = grid_search.best_estimator_
+    
+
+y_pred = tuned_model.predict(X_test)
+y_train_pred = tuned_model.predict(X_train)
+
+print('_____________________TUNED MODEL_____________________ ')
+# The coefficients
+print('Coefficients and Intercept are: ', tuned_model.coef_,"   ", tuned_model.intercept_,' respectively')
 # The mean squared error
 print('_________________###################____________________')
 print("Mean squared error for testing data: %.2f"
