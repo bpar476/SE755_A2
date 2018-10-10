6c219da19219ff74119aa026dc55966bd599c315
Fraser McIntosh <frasermcintosh1@gmail.com>
2018-10-10 13:29:21 +1300

    Add cmd args to landsat clf

--- src/landsat_classification.py
+++ src/landsat_classification.py
@@ -15,17 +15,35 @@ from sklearn.metrics import accuracy_score, precision_score
 from sklearn.externals import joblib
 
 # Argument processing
-parser = argparse.ArgumentParser(description='Machine learning algorithm for 2018 world cup data.')
-parser.add_argument('--test', help='path to additional features to test against. Path must be relative to the current directory. If supplied, results of predictions against this test data will be the last thing printed by this script (optional)')
-parser.add_argument('--re-train', help='will re-train the model and document cross validation process')
-parser.add_argument('--analysis', help='will run the model against test data and report performance')
+parser = argparse.ArgumentParser(description='Machine learning algorithm for traffic data. Options are available for re-training the model, testing the model with external data or running performance analysis. Note that only one option should be supplied at a time e.g. you should not run the script with both --test and --re-train.')
+
+parser.add_argument('--test', metavar='TEST_FILE', nargs=1, dest='test_file', help='path to additional features to test against. Path must be relative to the current directory. If supplied, results of predictions against this test data will be the last thing printed by this script (optional)')
+
+parser.add_argument('--re-train', action='store_const', const=True, default=False, help='will re-train the model and document cross validation process')
+
+parser.add_argument('--analysis', action='store_const', const=True, default=False, help='will run the model against test data and report performance')
+
+parser.add_argument('--test-dev', action='store_const', const=True, default=False, help='Will do a train-test and print the results to the command line using the dataset from Canvas. For development purpose only')
 
 parsed_args = parser.parse_args()
 
-MODEL_LOCATION = '../model/landsat_logistic_model'
-RESULTS_LOCATION = '../results/traffic_results.csv'
-PERFORMANCE_LOCATION = '../performance/landsat_logistic_regression.csv'
-CV_LOCATION = '../cv_results/landsat_logistic_cv.csv'
+MODEL_DIR = '../model'
+MODEL_NAME = 'landsat_logistic_model'
+
+RESULTS_DIR = '../results'
+RESULTS_NAME = 'landsat_results.csv'
+
+PERFORMANCE_DIR = '../performance'
+PERFORMANCE_NAME = 'landsat_logistic_regression.csv'
+
+CV_RESULTS_DIR = '../cv_results'
+CV_RESULTS_NAME = 'landsat_logistic_cv.csv'
+
+for directory in [MODEL_DIR, RESULTS_DIR, PERFORMANCE_DIR, CV_RESULTS_DIR]:
+    if not os.path.isdir(directory):
+        print("Making directory: {}".format(directory))
+        os.mkdir(directory)
+
 target_col = 36
 
 def train(save=True):
@@ -64,7 +82,7 @@ def train(save=True):
     best_params_ = grid_search.best_params_
     print(best_params_)
     
-    with open(CV_LOCATION, 'w') as outfile:
+    with open(os.path.join(CV_RESULTS_DIR, CV_RESULTS_NAME), 'w') as outfile:
         cvs.to_csv(path_or_buf=outfile, index=False)
         
     tuned_model = grid_search.best_estimator_
@@ -73,12 +91,15 @@ def train(save=True):
     untuned_model.fit(X_train, y_train)
     
     if save:
-        joblib.dump(tuned_model, MODEL_LOCATION)
-
+        print("Saving model in {}/{}".format(MODEL_DIR, MODEL_NAME))
+        joblib.dump(tuned_model, os.path.join(MODEL_DIR, MODEL_NAME))
+        
     return tuned_model, untuned_model, X_test, y_test
 
-def test(test_file):
-    model = joblib.load(MODEL_LOCATION)
+def test_extern(test_file):
+    print("Loading model from {}/{}".format(MODEL_DIR, MODEL_NAME))
+    model = joblib.load(os.path.join(MODEL_DIR, MODEL_NAME))
+    print("Loading test data from {}".format(test_file))
     test_data = pd.read_csv(test_file, header=None)
     
     # Preprocessing
@@ -92,8 +113,31 @@ def test(test_file):
 
     prediction = pd.DataFrame(data=model.predict(processed_features), columns=['Target'])
 
-    with open(RESULTS_LOCATION, 'w') as outfile:
+    with open(os.path.join(RESULTS_DIR,RESULTS_NAME), 'w') as outfile:
         prediction.to_csv(path_or_buf=outfile, index=False)
+        
+def test_dev():
+    tuned_model, untuned_model, X_test, y_test = train()
+
+    print("Making test predictions")
+    T_untuned = untuned_model.predict(X_test)
+    acc_untuned = (100*accuracy_score(y_test, T_untuned))
+    precision_untuned = (100*precision_score(y_test, T_untuned, average='weighted', labels=pd.unique(pd.unique(y_test))))
+    
+    #tuned score
+    T_tuned = tuned_model.predict(X_test)
+    acc_tuned = (100*accuracy_score(y_test, T_tuned))
+    precision_tuned = (100*precision_score(y_test, T_tuned,average='weighted', labels=pd.unique(pd.unique(y_test))))
+    
+    print(acc_untuned)
+    print('*******************************************************************')
+    print("The prediction accuracy (untuned) for all testing instances is : {:.2f}%.".format(acc_untuned))
+    print("The precision accuracy (untuned) for all testing instances is : {:.2f}%.".format(precision_untuned))
+    
+
+    print('*******************************************************************')
+    print("The prediction accuracy (tuned) for all testing instances is : {:.2f}%.".format(acc_tuned))
+    print("The prediction precision (tuned) for all testing instances is : {:.2f}%.".format(precision_tuned))
         
 def performance():
     num_trials = 1
@@ -148,11 +192,24 @@ def performance():
         
     results = results.append(untuned_results)
     results = results.append(tuned_results)
-    results.to_csv(path_or_buf=PERFORMANCE_LOCATION, index=False)
-
-train()
-#test('../datasets/landsat.csv')
-#performance()
+    results.to_csv(path_or_buf=os.path.join(PERFORMANCE_DIR, PERFORMANCE_NAME), index=False)
+    
+if __name__ == "__main__":
+    parsed_args = parser.parse_args()
+    if parsed_args.re_train:
+        print("Re-training model")
+        train()
+    elif parsed_args.analysis:
+        print("Doing performance analysis")
+        performance()
+    elif parsed_args.test_file != None:
+        print("Testing with external data from {}".format(parsed_args.test_file))
+        test_extern(parsed_args.test_file[0])
+    elif parsed_args.test_dev:
+        print("Running development test")
+        test_dev()
+    else:
+        parser.print_help()
 
 
 
