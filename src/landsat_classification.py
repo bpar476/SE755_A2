92f19bc6f8a49982c9fa11620e7b4dfd04ffa3aa
Ben Partridge <ben.partridge249@gmail.com>
2018-10-13 13:47:51 +1300

    Fix whitespace

--- src/landsat_classification.py
+++ src/landsat_classification.py
@@ -51,49 +51,49 @@ def train(save=True):
     data = pd.read_csv(fileName, header=None)
     columns_to_keep = [16, 17, 18, 19]
     target_col = 36
-    
+
     features = data.loc[:, columns_to_keep].copy()
     target = data.loc[:, target_col].copy()
-    
+
     full_pipeline = Pipeline([
             ('imputer', Imputer(strategy="median")),
             ('std_scaler', StandardScaler())
         ])
-    
+
     processed_features = pd.DataFrame(data=full_pipeline.fit_transform(features))
     sss = StratifiedShuffleSplit(n_splits=3, test_size=0.2, random_state=42)
     for train_index, test_index in sss.split(features, target):
                 train_ind,test_ind = train_index, test_index
-                
+
     X_train, X_test = processed_features.loc[train_ind], processed_features.loc[test_ind]
     y_train, y_test = target.loc[train_ind], target.loc[test_ind]
-             
+
     model = LogisticRegression()
-    
+
     param_grid = [
                 {'penalty': ['l1', 'l2'], 'C': [ 2**x for x in range(-6,6) ]},
             ]
-    
+
     grid_search = GridSearchCV(model, param_grid, cv=3, scoring='accuracy',verbose=1)
     grid_search.fit(X_train, y_train)
-    
+
     cv_results_ = grid_search.cv_results_
     cvs = pd.DataFrame(cv_results_)
     best_params_ = grid_search.best_params_
     print(best_params_)
-    
+
     with open(os.path.join(CV_RESULTS_DIR, CV_RESULTS_NAME), 'w') as outfile:
         cvs.to_csv(path_or_buf=outfile, index=False)
-        
+
     tuned_model = grid_search.best_estimator_
-    
+
     untuned_model = LogisticRegression()
     untuned_model.fit(X_train, y_train)
-    
+
     if save:
         print("Saving model in {}/{}".format(MODEL_DIR, MODEL_NAME))
         joblib.dump(tuned_model, os.path.join(MODEL_DIR, MODEL_NAME))
-        
+
     return tuned_model, untuned_model, X_test, y_test
 
 def test_extern(test_file):
@@ -101,7 +101,7 @@ def test_extern(test_file):
     model = joblib.load(os.path.join(MODEL_DIR, MODEL_NAME))
     print("Loading test data from {}".format(test_file))
     test_data = pd.read_csv(test_file, header=None)
-    
+
     # Preprocessing
     columns_to_keep = [16, 17, 18, 19]
     features = test_data.loc[:, columns_to_keep].copy()
@@ -115,7 +115,7 @@ def test_extern(test_file):
 
     with open(os.path.join(RESULTS_DIR,RESULTS_NAME), 'w') as outfile:
         prediction.to_csv(path_or_buf=outfile, index=False)
-        
+
 def test_dev():
     tuned_model, untuned_model, X_test, y_test = train()
 
@@ -123,56 +123,56 @@ def test_dev():
     T_untuned = untuned_model.predict(X_test)
     acc_untuned = (100*accuracy_score(y_test, T_untuned))
     f1_untuned = (f1_score(y_test, T_untuned, average='weighted', labels=pd.unique(pd.unique(y_test))))
-    
+
     #tuned score
     T_tuned = tuned_model.predict(X_test)
     acc_tuned = (100*accuracy_score(y_test, T_tuned))
     f1_tuned = (f1_score(y_test, T_tuned,average='weighted', labels=pd.unique(pd.unique(y_test))))
-    
+
     print(acc_untuned)
     print('*******************************************************************')
     print("The prediction accuracy (untuned) for all testing instances is : {:.2f}%.".format(acc_untuned))
     print("The f1 score (untuned) for all testing instances is : {:.2f}.".format(f1_untuned))
-    
+
 
     print('*******************************************************************')
     print("The prediction accuracy (tuned) for all testing instances is : {:.2f}%.".format(acc_tuned))
     print("The f1 score (tuned) for all testing instances is : {:.2f}.".format(f1_tuned))
-        
+
 def performance():
     num_trials = 1
     acc_untuned = []
     acc_tuned = []
     f1_untuned = []
     f1_tuned = []
-    
+
     # calculate performance over several trials
     for x in range(0, num_trials):
         print("trial {:d}".format(x))
         tuned_model, untuned_model, X_test, y_test = train(False)
-        
+
         #untuned score
         T_untuned = untuned_model.predict(X_test)
         acc_untuned.append(100*accuracy_score(y_test, T_untuned))
         f1_untuned.append(f1_score(y_test, T_untuned, average='weighted', labels=pd.unique(pd.unique(y_test))))
-        
+
         #tuned score
         T_tuned = tuned_model.predict(X_test)
         acc_tuned.append(100*accuracy_score(y_test, T_tuned))
         f1_tuned.append(f1_score(y_test, T_tuned,average='weighted', labels=pd.unique(pd.unique(y_test))))
-    
-        
+
+
     #print(classification_report(self.y_test_matrix, T_tuned, target_names=pd.unique(pd.unique(self.target))))
     #report = pd.DataFrame(classification_report(self.y_test_matrix, T_tuned, target_names=pd.unique(pd.unique(self.target))))
-   
-    
+
+
     # Take mean of all the trials
     acc_untuned_av = pd.DataFrame(acc_untuned).mean()
     f1_untuned_av =  pd.DataFrame(f1_untuned).mean()
-    
+
     acc_tuned_av = pd.DataFrame(acc_tuned).mean()
     f1_tuned_av =  pd.DataFrame(f1_tuned).mean()
-    
+
     print(acc_untuned)
     print('({:d} trials)'.format(num_trials))
     print('*******************************************************************')
@@ -182,19 +182,19 @@ def performance():
     print('*******************************************************************')
     print("The average prediction accuracy (tuned) for all testing instances is : {:.2f}%.".format(acc_tuned_av.iloc[0]))
     print("The average f1 score (tuned) for all testing instances is : {:.2f}.".format(f1_tuned_av.iloc[0]))
-    
+
     ## Print all this stuff to a file
     headers = ['Model', 'Model Tuning', 'Accuracy (%)', 'f1 (%)']
     model_name = 'logistic_regression'
     untuned_results = pd.DataFrame([[model_name, 'Untuned', acc_untuned_av.iloc[0], f1_untuned_av.iloc[0]]], columns=headers)
     tuned_results = pd.DataFrame([[model_name, 'Tuned', acc_tuned_av.iloc[0], f1_tuned_av.iloc[0]]], columns=headers)
-    
+
     results = pd.DataFrame(columns=headers)
-        
+
     results = results.append(untuned_results)
     results = results.append(tuned_results)
     results.to_csv(path_or_buf=os.path.join(PERFORMANCE_DIR, PERFORMANCE_NAME), index=False)
-    
+
 if __name__ == "__main__":
     parsed_args = parser.parse_args()
     if parsed_args.re_train:
@@ -213,14 +213,3 @@ if __name__ == "__main__":
         parser.print_help()
 
 
-    
-        
-            
-        
-
-
-
-        
-        
-        
-    
